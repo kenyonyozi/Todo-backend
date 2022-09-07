60ec6630da14ba06513793785d7db07122606c1d
kenyonyozi <kenyonyozibwengye@gmail.com>
2022-09-07 23:08:26 +0300

    add get api

--- app.js
+++ app.js
@@ -10,13 +10,14 @@ const { WELCOME_MESSAGE, DATABASE_URL } = process.env;
 
 const app = express();
 
-
+//calling items route
+const itemsRoutes = require("./routes/items.js");
 
 app.use(bodyParser.json()); // for parsing application/json
 // it helps log into the syetem
 app.use(cors());
 
-;
+app.use("/items", itemsRoutes);
 
 //spining database
 mongoose
